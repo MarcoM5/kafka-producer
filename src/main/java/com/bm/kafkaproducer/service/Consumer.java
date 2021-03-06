b0e3e6dde8c3f685d5977cc16ddb42bf80513b97
Marco <marco@blackmesa.in>
2021-03-06 12:17:42 +0000

    First commit

---
+++ src/main/java/com/bm/kafkaproducer/service/Consumer.java
@@ -0,0 +1,18 @@
+package com.bm.kafkaproducer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+    @KafkaListener(topics = "super-sumo", groupId = "group_id")
+    public void consume(String message) {
+        logger.info(String.format("### -> Consumed message -> %s", message));
+    }
+
+}
